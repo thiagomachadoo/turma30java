6eb3cb1f9157e24350f1ced55b2b3a1d24e02005
Thiago Machado <thiagodasilvamachadoo44@gmail.com>
2021-09-08 20:51:33 -0300

    Criando a classe repository para que seja possivel definir de qual URL sera puxado, alem do que complementando com CrossOrigin fazendo esse parametro ser acessado por qualquer servidor, deixando com a injeção de dependências sendo usado como ponte, sendo assim acessada pelo controlador

--- lojaGames/lojagames/src/main/java/com/lojaGames/lojagames/repository/PostagemRepository.java
+++ lojaGames/lojagames/src/main/java/com/lojaGames/lojagames/repository/PostagemRepository.java
@@ -1,4 +1,13 @@
 package com.lojaGames.lojagames.repository;
 
-public interface PostagemRepository {
+import com.lojaGames.lojagames.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository//Indicar que essa classe se trata de um repositório
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+    public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+    //buscar todos pelo titulo = (nome do atributo da minha entidade)
 }
